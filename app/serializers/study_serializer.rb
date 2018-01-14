c34d08b61b6eb171354d249f7ed331b8a0402bb5
jolavb <julianb@bu.edu>
2018-01-14 17:43:18 -0500

    Lock all but index and show routes for studies

--- app/serializers/study_serializer.rb
+++ app/serializers/study_serializer.rb
@@ -1,5 +1,8 @@
 class StudySerializer < ActiveModel::Serializer
-  attributes :id, :official_title, :brief_title, :comments
+  attributes :id, :official_title, :brief_title, :comments, :study_type,
+             :baseline_population, :overall_status, :phase, :enrollment,
+             :enrollment_type, :source, :limitations_and_caveats, :is_fda_regulated_drug,
+             :is_fda_regulated_device, :target_duration, :primary_completion_date, :nlm_download_date_description
 
   def comments
     object.comments.pluck(:id)
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@
 
 Rails.application.routes.draw do
   resources :comments
-  resources :studies
-  resources :examples, except: %i[new edit]
+  resources :studies, only: %i[index show]
+  # resources :examples, except: %i[new edit]
   post '/sign-up' => 'users#signup'
   post '/sign-in' => 'users#signin'
   delete '/sign-out/:id' => 'users#signout'
