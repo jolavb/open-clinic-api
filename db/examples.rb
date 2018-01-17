fb0cc342c877749e4af03824fcdf8fcbe2502b61
jolavb <julianb@bu.edu>
2018-01-16 20:57:02 -0500

    Add seed data for production database
    1. Add methods for exporting studies and dependent tables to
    csv.
    2. Add methods for loading exported data into production database

--- db/examples.rb
+++ db/examples.rb
@@ -1,20 +1,52 @@
 # frozen_string_literal: true
 
-# This file should contain all the record creation needed to experiment with
-# your app during development.
-#
-# The data can then be loaded with the rake db:examples (or created alongside
-# the db with db:nuke_pave).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'csv'
 
-# %w(antony jeff matt jason).each do |name|
-#   email = "#{name}@#{name}.com"
-#   next if User.exists? email: email
-#   User.create!(email: email,
-#                password: 'abc123',
-#                password_confirmation: nil)
-# end
+@models = [Study, Sponsor, ResponsibleParty]
+@dependents = [Sponsor, ResponsibleParty, Comment]
+
+def export_studies(limit)
+  CSV.open("db/production-examples/Study.csv", "a+") do |csv|
+    Study.order("RANDOM()").limit(limit).each do |study|
+      csv << study.attributes.values
+      @dependents.each do |dependent|
+        load_dependent(dependent, study)
+      end
+    end
+  end
+end
+
+def load_dependent(dependent, study)
+  CSV.open("db/production-examples/#{dependent}.csv", "a+") do |csv|
+    dependent.where(nct_id: study[:nct_id]).each do |instance|
+      csv << instance.attributes.values
+    end
+  end
+end
+
+def set_files
+  @models.each do |model|
+    CSV.open("db/production-examples/#{model}.csv", "wb") do |csv|
+      csv << model.attribute_names
+    end
+  end
+end
+
+def test(limit)
+  CSV.open("db/production-examples/Comment.csv", "wb") do |csv|
+    csv << Comment.attribute_names
+    Comment.order("RANDOM()").limit(limit).each do |comment|
+      csv << comment.attributes.values
+    end
+  end
+end
+
+
+def export
+  set_files
+  export_studies(10)
+end
+
+test(200)
+
+# export
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,30 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+require 'csv'
+def load_files(model_file)
+  model = model_file.chomp('.csv').constantize
+  CSV.foreach("db/production-examples/#{model_file}", headers: true) do |row|
+    model.create(row.to_h)
+  end
+end
+
+# DO NOT INVOKE find_files in Development!!
+def find_files
+  Dir.foreach('db/production-examples') do |fileName|
+    next if fileName == '.' or fileName  == '..'
+    load_files(fileName)
+  end
+end
+
+
+def test
+  Comment.destroy_all
+  CSV.foreach('db/production-examples/Comment.csv', headers: true) do |row|
+    Comment.create(row.to_h)
+  end
+end
+
+load_files
